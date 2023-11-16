50ffa6ccb1c3bab25822b3242538aa1220813c4a
Jose <enriquesais123@gmail.com>
2023-11-16 15:06:42 -0500

    inicio

---
+++ database/db.sql
@@ -0,0 +1,20 @@
+-- creando la base de datos
+
+CREATE DATABASE crudjs;
+
+-- utilizando la base de datos
+
+use crudjs;
+
+CREATE TABLE customer(
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    phone VARCHAR(15),
+);
+
+
+SHOW TABLES;
+
+
+describe customer;
